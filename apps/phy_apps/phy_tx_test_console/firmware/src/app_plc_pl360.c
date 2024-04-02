c42627687e7cb217b01115bb034d3fbb27d164e0
Cristian Guemes <cristian.guemes@microchip.com>
2024-04-02 13:42:35 +0200

    SMS-3918: Create new PHY TX Console application for M&M

---
+++ apps/phy_apps/phy_tx_test_console/firmware/src/app_plc_pl360.c
@@ -0,0 +1,480 @@
+/*
+Copyright (C) 2022, Microchip Technology Inc., and its subsidiaries. All rights reserved.
+
+The software and documentation is provided by microchip and its contributors
+"as is" and any express, implied or statutory warranties, including, but not
+limited to, the implied warranties of merchantability, fitness for a particular
+purpose and non-infringement of third party intellectual property rights are
+disclaimed to the fullest extent permitted by law. In no event shall microchip
+or its contributors be liable for any direct, indirect, incidental, special,
+exemplary, or consequential damages (including, but not limited to, procurement
+of substitute goods or services; loss of use, data, or profits; or business
+interruption) however caused and on any theory of liability, whether in contract,
+strict liability, or tort (including negligence or otherwise) arising in any way
+out of the use of the software and documentation, even if advised of the
+possibility of such damage.
+
+Except as expressly permitted hereunder and subject to the applicable license terms
+for any third-party software incorporated in the software and any applicable open
+source software license terms, no license or other rights, whether express or
+implied, are granted under any patent or other intellectual property rights of
+Microchip or any third party.
+*/
+
+/*******************************************************************************
+  MPLAB Harmony Application Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    app_plc_pl360.c
+
+  Summary:
+    This file contains the source code for the MPLAB Harmony application.
+
+  Description:
+    This file contains the source code for the MPLAB Harmony application.  It
+    implements the logic of the application's state machine and it may call
+    API routines of other MPLAB Harmony modules in the system, such as drivers,
+    system services, and middleware.  However, it does not call any of the
+    system interfaces (such as the "Initialize" and "Tasks" functions) of any of
+    the modules in the system or make any assumptions about when those functions
+    are called.  That is the responsibility of the configuration-specific system
+    files.
+ *******************************************************************************/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <string.h>
+#include "definitions.h"
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Global Data Definitions
+// *****************************************************************************
+// *****************************************************************************
+#define DRV_PLC_PHY_INDEX_0   0
+#define APP_PLC_CONFIG_KEY  0xA55A
+
+/* PLC Driver Initialization Data (initialization.c) */
+extern DRV_PLC_PHY_INIT drvPlcPhyInitData;
+
+// *****************************************************************************
+/* Application Data
+
+  Summary:
+    Holds application data
+
+  Description:
+    This structure holds the application's data.
+
+  Remarks:
+    This structure should be initialized by the APP_Initialize function.
+
+    Application strings and buffers are be defined outside this structure.
+*/
+
+CACHE_ALIGN APP_PLC_DATA appPlc;
+CACHE_ALIGN APP_PLC_DATA_TX appPlcTx;
+
+static CACHE_ALIGN uint8_t appPlcPibDataBuffer[CACHE_ALIGNED_SIZE_GET(APP_PLC_PIB_BUFFER_SIZE)];
+static CACHE_ALIGN uint8_t appPlcTxDataBuffer[CACHE_ALIGNED_SIZE_GET(APP_PLC_BUFFER_SIZE)];
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Callback Functions
+// *****************************************************************************
+// *****************************************************************************
+void Timer1_Callback (uintptr_t context)
+{
+    appPlc.tmr1Expired = true;
+}
+
+void Timer2_Callback (uintptr_t context)
+{
+    appPlc.tmr2Expired = true;
+}
+
+static void APP_PLC_ExceptionCb(DRV_PLC_PHY_EXCEPTION exceptionObj, uintptr_t context )
+{
+    /* Avoid warning */
+    (void)context;
+
+    /* Clear App flag */
+    appPlc.waitingTxCfm = false;
+    /* Restore TX configuration */
+    appPlc.state = APP_PLC_STATE_READ_CONFIG;
+}
+
+static void APP_PLC_DataCfmCb(DRV_PLC_PHY_TRANSMISSION_CFM_OBJ *cfmObj, uintptr_t context )
+{
+    /* Avoid warning */
+    (void)context;
+
+    /* Update App flags */
+    appPlc.waitingTxCfm = false;
+
+    /* Handle result of transmission : Show it through Console */
+    switch(cfmObj->result)
+    {
+        case DRV_PLC_PHY_TX_RESULT_PROCESS:
+            APP_CONSOLE_Print("...DRV_PLC_PHY_TX_RESULT_PROCESS\r\n");
+            break;
+        case DRV_PLC_PHY_TX_RESULT_SUCCESS:
+            APP_CONSOLE_Print("...DRV_PLC_PHY_TX_RESULT_SUCCESS\r\n");
+            break;
+        case DRV_PLC_PHY_TX_RESULT_INV_LENGTH:
+            APP_CONSOLE_Print("...DRV_PLC_PHY_TX_RESULT_INV_LENGTH\r\n");
+            break;
+        case DRV_PLC_PHY_TX_RESULT_BUSY_CH:
+            APP_CONSOLE_Print("...DRV_PLC_PHY_TX_RESULT_BUSY_CH\r\n");
+            break;
+        case DRV_PLC_PHY_TX_RESULT_BUSY_TX:
+            APP_CONSOLE_Print("...DRV_PLC_PHY_TX_RESULT_BUSY_TX\r\n");
+            break;
+        case DRV_PLC_PHY_TX_RESULT_BUSY_RX:
+            APP_CONSOLE_Print("...DRV_PLC_PHY_TX_RESULT_BUSY_RX\r\n");
+            break;
+        case DRV_PLC_PHY_TX_RESULT_TIMEOUT:
+            APP_CONSOLE_Print("...DRV_PLC_PHY_TX_RESULT_TIMEOUT\r\n");
+            break;
+        case DRV_PLC_PHY_TX_CANCELLED:
+            APP_CONSOLE_Print("...DRV_PLC_PHY_TX_CANCELLED\r\n");
+            break;
+        case DRV_PLC_PHY_TX_RESULT_NO_TX:
+            APP_CONSOLE_Print("...DRV_PLC_PHY_TX_RESULT_NO_TX\r\n");
+            break;
+    }
+}
+
+static void APP_PLC_DataIndCb( DRV_PLC_PHY_RECEPTION_OBJ *indObj, uintptr_t context )
+{
+    /* Avoid warning */
+    (void)context;
+
+    if (indObj->dataLength)
+    {
+        USER_PLC_IND_LED_On();
+        /* Start signal timer */
+        appPlc.tmr2Handle = SYS_TIME_CallbackRegisterMS(Timer2_Callback, 0, LED_PLC_RX_MSG_RATE_MS, SYS_TIME_SINGLE);
+    }
+}
+
+/*******************************************************************************
+  Function:
+    void APP_PLC_PL360_Initialize(void)
+
+  Remarks:
+    See prototype in app_plc.h.
+ */
+void APP_PLC_PL360_Initialize ( void )
+{
+    /* IDLE state is used to signal when application is started */
+    appPlc.state = APP_PLC_STATE_IDLE;
+
+    /* Init flags of PLC transmission */
+    appPlc.waitingTxCfm = false;
+
+    /* Init PLC PIB buffer */
+    appPlc.plcPIB.pData = appPlcPibDataBuffer;
+
+    /* Init PLC TX Buffer */
+    appPlcTx.plcPhyTx.pTransmitData = appPlcTxDataBuffer;
+
+    /* Init Timer handler */
+    appPlc.tmr1Handle = SYS_TIME_HANDLE_INVALID;
+    appPlc.tmr2Handle = SYS_TIME_HANDLE_INVALID;
+    appPlc.tmr1Expired = false;
+    appPlc.tmr2Expired = false;
+
+    /* Init signalling */
+    appPlc.signalResetCounter = LED_RESET_BLINK_COUNTER;
+
+}
+
+/******************************************************************************
+  Function:
+    void APP_PLC_PL360_Tasks ( void )
+
+  Remarks:
+    See prototype in app_plc.h.
+ */
+
+void APP_PLC_PL360_Tasks ( void )
+{
+    /* Signalling: LED Toggle */
+    if (appPlc.tmr1Expired)
+    {
+        appPlc.tmr1Expired = false;
+        USER_BLINK_LED_Toggle();
+
+        if (appPlc.signalResetCounter)
+        {
+            appPlc.signalResetCounter--;
+        }
+    }
+
+    /* Signalling: PLC RX */
+    if (appPlc.tmr2Expired)
+    {
+        appPlc.tmr2Expired = false;
+        USER_PLC_IND_LED_Off();
+    }
+
+    /* Check the application's current state. */
+    switch ( appPlc.state )
+    {
+        case APP_PLC_STATE_IDLE:
+        {
+            /* Signalling when the application is starting */
+            if (appPlc.signalResetCounter)
+            {
+                if (appPlc.tmr1Handle == SYS_TIME_HANDLE_INVALID)
+                {
+                    /* Init Timer to handle blinking led */
+                    appPlc.tmr1Handle = SYS_TIME_CallbackRegisterMS(Timer1_Callback, 0, LED_RESET_BLINK_RATE_MS, SYS_TIME_PERIODIC);
+                }
+            }
+            else
+            {
+                SYS_TIME_TimerDestroy(appPlc.tmr1Handle);
+                appPlc.tmr1Handle = SYS_TIME_HANDLE_INVALID;
+
+                /* Read configuration from NVM memory */
+                appPlc.state = APP_PLC_STATE_READ_CONFIG;
+            }
+            break;
+        }
+
+        case APP_PLC_STATE_READ_CONFIG:
+        {
+            if (appNvm.state == APP_NVM_STATE_CMD_WAIT)
+            {
+                appNvm.pData = (uint8_t*)&appPlcTx;
+                appNvm.dataLength = sizeof(appPlcTx);
+                appNvm.state = APP_NVM_STATE_READ_MEMORY;
+
+                appPlc.state = APP_PLC_STATE_CHECK_CONFIG;
+            }
+            break;
+        }
+
+        case APP_PLC_STATE_CHECK_CONFIG:
+        {
+            if (appNvm.state == APP_NVM_STATE_CMD_WAIT)
+            {
+                uint8_t* pData;
+                uint8_t dataValue;
+                uint8_t length;
+                
+                if (appPlcTx.configKey != APP_PLC_CONFIG_KEY)
+                {
+                    /* Set configuration by default */
+                    appPlcTx.configKey = APP_PLC_CONFIG_KEY;
+                    appPlcTx.plcPhyVersion = 0;
+                    appPlcTx.txImpedance = HI_STATE;
+                    appPlcTx.txAuto = 0;
+                    appPlcTx.plcPhyTx.timeIni = 1000000;
+                    appPlcTx.plcPhyTx.attenuation = 0;
+                    appPlcTx.plcPhyTx.mode = TX_MODE_RELATIVE;
+                    appPlcTx.plcPhyTx.nbFrame = 0;
+                    appPlcTx.plcPhyTx.dataLength = 64;
+                    appPlcTx.plcPhyTx.pTransmitData = appPlcTxDataBuffer;
+                    
+
+                    /* Clear Transmission flag */
+                    appPlcTx.inTx = false;
+                }
+                
+                /* Set Fixed data */
+                dataValue = 0x30;
+                pData = appPlcTxDataBuffer;
+                while(length--)
+                {
+                    *pData++ = (uint8_t)dataValue++;
+                    if (dataValue == 0x3A)
+                    {
+                        dataValue = 0x30;
+                    }
+                }
+
+                /* Initialize PLC driver */
+                appPlc.state = APP_PLC_STATE_INIT;
+            }
+            break;
+        }
+
+        case APP_PLC_STATE_WRITE_CONFIG:
+        {
+            if (appNvm.state == APP_NVM_STATE_CMD_WAIT)
+            {
+                appNvm.pData = (uint8_t*)&appPlcTx;
+                appNvm.dataLength = sizeof(appPlcTx);
+                appNvm.state = APP_NVM_STATE_WRITE_MEMORY;
+
+                appPlc.state = APP_PLC_STATE_WAIT_CONFIG;
+            }
+            break;
+        }
+
+        case APP_PLC_STATE_WAIT_CONFIG:
+        {
+            if (appNvm.state == APP_NVM_STATE_CMD_WAIT)
+            {
+                if (appPlcTx.inTx)
+                {
+                    appPlc.state = APP_PLC_STATE_TX;
+                }
+                else
+                {
+                    appPlc.state = APP_PLC_STATE_WAITING;
+                }
+            }
+            break;
+        }
+
+        case APP_PLC_STATE_INIT:
+        {
+            /* Open PLC driver */
+            appPlc.drvPl360Handle = DRV_PLC_PHY_Open(DRV_PLC_PHY_INDEX_0, NULL);
+
+            if (appPlc.drvPl360Handle != DRV_HANDLE_INVALID)
+            {
+                appPlc.state = APP_PLC_STATE_OPEN;
+            }
+            else
+            {
+                appPlc.state = APP_PLC_STATE_ERROR;
+            }
+            break;
+        }
+
+        case APP_PLC_STATE_OPEN:
+        {
+            /* Check PLC transceiver */
+            if (DRV_PLC_PHY_Status(DRV_PLC_PHY_INDEX_0) == SYS_STATUS_READY)
+            {
+                DRV_PLC_PHY_PIB_OBJ pibObj;
+                uint32_t version;
+
+                /* Configure PLC callbacks */
+                DRV_PLC_PHY_ExceptionCallbackRegister(appPlc.drvPl360Handle, APP_PLC_ExceptionCb, DRV_PLC_PHY_INDEX_0);
+                DRV_PLC_PHY_TxCfmCallbackRegister(appPlc.drvPl360Handle, APP_PLC_DataCfmCb, DRV_PLC_PHY_INDEX_0);
+                DRV_PLC_PHY_DataIndCallbackRegister(appPlc.drvPl360Handle, APP_PLC_DataIndCb, DRV_PLC_PHY_INDEX_0);
+
+                /* Apply PLC coupling configuration */
+                SRV_PCOUP_Set_Config(appPlc.drvPl360Handle, SRV_PLC_PCOUP_MAIN_BRANCH);
+
+                /* Init Timer to handle blinking led */
+                appPlc.tmr1Handle = SYS_TIME_CallbackRegisterMS(Timer1_Callback, 0, LED_BLINK_RATE_MS, SYS_TIME_PERIODIC);
+
+                /* Get PLC PHY version */
+                pibObj.id = PLC_ID_VERSION_NUM;
+                pibObj.length = 4;
+                pibObj.pData = (uint8_t *)&version;
+                DRV_PLC_PHY_PIBGet(appPlc.drvPl360Handle, &pibObj);
+
+                if (version == appPlcTx.plcPhyVersion)
+                {
+                    if (appPlcTx.inTx)
+                    {
+                        /* Previous Transmission state */
+                        appPlc.state = APP_PLC_STATE_TX;
+                    }
+                    else
+                    {
+                        /* Nothing To Do */
+                        appPlc.state = APP_PLC_STATE_WAITING;
+                    }
+                }
+                else
+                {
+                    appPlcTx.plcPhyVersion = version;
+
+                    /* Store configuration in NVM memory */
+                    appPlc.state = APP_PLC_STATE_WRITE_CONFIG;
+                }
+            }
+            break;
+        }
+
+        case APP_PLC_STATE_WAITING:
+        {
+            break;
+        }
+
+        case APP_PLC_STATE_TX:
+        {
+            if (!appPlcTx.inTx)
+            {
+                DRV_PLC_PHY_PIB_OBJ pibObj;
+
+                /* Apply TX configuration */
+                /* Set Autodetect Mode */
+                pibObj.id = PLC_ID_CFG_AUTODETECT_IMPEDANCE;
+                pibObj.length = 1;
+                pibObj.pData = (uint8_t *)&appPlcTx.txAuto;
+                DRV_PLC_PHY_PIBSet(appPlc.drvPl360Handle, &pibObj);
+                /* Set Impedance Mode */
+                pibObj.id = PLC_ID_CFG_IMPEDANCE;
+                pibObj.length = 1;
+                pibObj.pData = (uint8_t *)&appPlcTx.txImpedance;
+                DRV_PLC_PHY_PIBSet(appPlc.drvPl360Handle, &pibObj);
+
+                /* Set Transmission Mode */
+                appPlcTx.plcPhyTx.mode = TX_MODE_RELATIVE;
+
+                /* Set Transmission flag */
+                appPlcTx.inTx = true;
+
+                /* Store TX configuration */
+                appPlc.state = APP_PLC_STATE_WRITE_CONFIG;
+            }
+            else
+            {
+                if (!appPlc.waitingTxCfm)
+                {
+                    appPlc.waitingTxCfm = true;
+                    /* Send PLC message */
+                    DRV_PLC_PHY_TxRequest(appPlc.drvPl360Handle, &appPlcTx.plcPhyTx);
+                }
+            }
+
+            break;
+        }
+
+        case APP_PLC_STATE_STOP_TX:
+        {
+            /* Clear Transmission flag */
+            appPlcTx.inTx = false;
+
+            /* Store TX configuration */
+            appPlc.state = APP_PLC_STATE_WRITE_CONFIG;
+
+            /* Cancel last transmission */
+            if (appPlc.waitingTxCfm)
+            {
+                /* Send PLC Cancel message */
+                appPlcTx.plcPhyTx.mode = TX_MODE_CANCEL | TX_MODE_RELATIVE;
+                DRV_PLC_PHY_TxRequest(appPlc.drvPl360Handle, &appPlcTx.plcPhyTx);
+            }
+            break;
+        }
+
+        /* The default state should never be executed. */
+        default:
+        {
+            break;
+        }
+    }
+}
+
+
+/*******************************************************************************
+ End of File
+ */
