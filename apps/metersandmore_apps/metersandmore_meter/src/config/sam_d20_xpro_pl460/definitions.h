3de6ec6ea1fdbddf4d56e60e33fcb2f4461329ca
Luis Lacasa <luis.lacasa@microchip.com>
2025-02-11 13:08:43 +0100

    MH3-110105: Regenerate stack projects for SAMD20-XPRO

--- apps/metersandmore_apps/metersandmore_meter/src/config/sam_d20_xpro_pl460/definitions.h
+++ apps/metersandmore_apps/metersandmore_meter/src/config/sam_d20_xpro_pl460/definitions.h
@@ -52,10 +52,10 @@
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
 #include "peripheral/adc/plib_adc.h"
 #include "peripheral/tc/plib_tc0.h"
-#include "system/time/sys_time.h"
 #include "driver/plc/phy/drv_plc_phy_definitions.h"
 #include "driver/plc/phy/drv_plc_phy.h"
 #include "driver/plc/phy/drv_plc_phy_comm.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/ports/sys_ports.h"
 #include "osal/osal.h"
@@ -207,9 +207,9 @@ Remarks:
 
 typedef struct
 {
-    SYS_MODULE_OBJ  sysTime;
 
     SYS_MODULE_OBJ drvPlcPhy;
+    SYS_MODULE_OBJ  sysTime;
     SYS_MODULE_OBJ  sysConsole0;
 
     SYS_MODULE_OBJ  sysDebug;
