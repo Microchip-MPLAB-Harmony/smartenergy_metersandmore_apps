21d84b2718a61d98e3c073330d9c20c74b64e72b
Luis Lacasa <luis.lacasa@microchip.com>
2025-02-12 12:42:16 +0100

    MH3-110109: Increase TX buffer of UART used by console (FreeRTOS projects)

--- apps/modem_app/src/config/pic32cx_mtsh_db_pl460_freertos/definitions.h
+++ apps/modem_app/src/config/pic32cx_mtsh_db_pl460_freertos/definitions.h
@@ -50,14 +50,14 @@
 #include <stdbool.h>
 #include "service/pcoup/srv_pcoup.h"
 #include "peripheral/adc/plib_adc.h"
-#include "peripheral/tc/plib_tc0.h"
 #include "peripheral/flexcom/usart/plib_flexcom6_usart.h"
+#include "peripheral/tc/plib_tc0.h"
 #include "peripheral/flexcom/usart/plib_flexcom0_usart.h"
 #include "peripheral/flexcom/spi/master/plib_flexcom1_spi_master.h"
+#include "system/time/sys_time.h"
 #include "driver/plc/phy/drv_plc_phy_definitions.h"
 #include "driver/plc/phy/drv_plc_phy.h"
 #include "driver/plc/phy/drv_plc_phy_comm.h"
-#include "system/time/sys_time.h"
 #include "peripheral/trng/plib_trng.h"
 #include "stack/metersandmore/mmhi/mmhi.h"
 #include "stack/metersandmore/mmhi/mmhi_definitions.h"
@@ -77,8 +77,8 @@
 #include "service/pcrc/srv_pcrc.h"
 #include "peripheral/sefc/plib_sefc0.h"
 #include "peripheral/sefc/plib_sefc1.h"
-#include "stack/metersandmore/pal/pal.h"
 #include "service/pvddmon/srv_pvddmon.h"
+#include "stack/metersandmore/pal/pal.h"
 #include "system/console/sys_console.h"
 #include "system/console/src/sys_console_uart_definitions.h"
 #include "FreeRTOS.h"
@@ -217,9 +217,9 @@ Remarks:
 
 typedef struct
 {
+    SYS_MODULE_OBJ  sysTime;
 
     SYS_MODULE_OBJ drvPlcPhy;
-    SYS_MODULE_OBJ  sysTime;
     SYS_MODULE_OBJ metersandmoreHostInterface;
 
     SYS_MODULE_OBJ  sysConsole0;
